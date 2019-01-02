443e7801931e8113181b7c94f5dad4f43274d0fb
changyangzhang <changyang.zhang@accedo.tv>
2019-01-02 23:14:23 +0100

    fetch

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyC-HKVnuGY5eitjUBxOQivi0pyRwV-K55M",
-    authDomain: "expensify-a1e4d.firebaseapp.com",
-    databaseURL: "https://expensify-a1e4d.firebaseio.com",
-    projectId: "expensify-a1e4d",
-    storageBucket: "expensify-a1e4d.appspot.com",
-    messagingSenderId: "450963661034"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-   name: 'Changyang Zhang'
-});
+const database = firebase.database();
+
+export { firebase, database as default };
--- expensify-app/src/tests/reducers/expenses.test.js
+++ expensify-app/src/tests/reducers/expenses.test.js
@@ -1,39 +1,76 @@
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
 
-test('should set default state', () =>{
-    const state = expensesReducer(undefined, {type:'@@INIT'});
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
     expect(state).toEqual([]);
 });
 
-test('should remove expense by id', () =>{
-    const state = expensesReducer(expenses, {type:'REMOVE_EXPENSE', id: expenses[1].id});
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
     expect(state).toEqual([expenses[0], expenses[2]]);
 });
 
-test('should not remove expense by id', () =>{
-    const state = expensesReducer(expenses, {type:'REMOVE_EXPENSE', id: '456'});
-    expect(state).toEqual([expenses[0], expenses[1],expenses[2]]);
+test('should not remove expenses if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
 });
 
-test('should edit expense', () =>{
+test('should add an expense', () => {
     const expense = {
         id: '109',
-        description:'Laptop',
-        note:'',
+        description: 'Laptop',
+        note: '',
         createdAt: 20000,
         amount: 29500
     };
-    const state = expensesReducer(expenses, {type:'ADD_EXPENSE', expense});
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense
+    };
+    const state = expensesReducer(expenses, action);
     expect(state).toEqual([...expenses, expense]);
 });
 
-test('should edit expense', () =>{
-    const state = expensesReducer(expenses, {type:'EDIT_EXPENSE', id: expenses[1].id, updates:{amount: 122}});
-    expect(state[1].amount).toEqual(122);
+test('should edit an expense', () => {
+    const amount = 122000;
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[1].id,
+        updates: {
+            amount
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[1].amount).toBe(amount);
 });
 
-test('should not edit expense', () =>{
-    const state = expensesReducer(expenses, {type:'EDIT_EXPENSE', id: '-1' , updates:{amount: 122}});
+test('should not edit an expense if id not found', () => {
+    const amount = 122000;
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: '-1',
+        updates: {
+            amount
+        }
+    };
+    const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);
+});
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
 });
